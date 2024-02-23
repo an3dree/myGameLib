d3a66088552799a4bba964540bc9bd6a401fadf0
André Souza <an3dree@gmail.com>
2024-02-23 09:23:52 -0300

    search page route

--- my-game-lib/src/routes/Routes.tsx
+++ my-game-lib/src/routes/Routes.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import App from '../App';
+import SearchPage from '../pages/search/SearchPage';
 
 const AppRoutes: React.FC = () => {
     return (
@@ -9,6 +10,7 @@ const AppRoutes: React.FC = () => {
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/home" element={<Home />} />
+                <Route path="/search" element={<SearchPage />} />
             </Routes>
         </Router>
     );
