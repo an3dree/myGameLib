b9b80aef79857b622580cc675286ecad86b499cf
André Souza <an3dree@gmail.com>
2024-02-23 09:24:45 -0300

    search game logic

--- my-game-lib/src/services/SearchGame.ts
+++ my-game-lib/src/services/SearchGame.ts
@@ -4,10 +4,14 @@ import { rawgApiKey } from "../utils/keys";
 const API_KEY = rawgApiKey;
 const BASE_URL = 'https://api.rawg.io/api';
 
-const searchGames = async (searchTerm: string): Promise<SearchGameResult[]> => {
+const searchGames = async (
+    searchTerm: string,
+    page: number = 1,
+    pageSize: number = 20
+): Promise<{ results: SearchGameResult[]; totalCount: number }> => {
     try {
         // Construa a URL de busca com o termo de pesquisa fornecido
-        const url = `${BASE_URL}/games?key=${API_KEY}&search=${searchTerm}`;
+        const url = `${BASE_URL}/games?key=${API_KEY}&search=${searchTerm}?order=metacritic&page=${page}&page_size=${pageSize}`;
 
         // Faça a solicitação GET para a API
         const response = await fetch(url);
@@ -19,12 +23,14 @@ const searchGames = async (searchTerm: string): Promise<SearchGameResult[]> => {
 
         // Analise a resposta para obter os dados dos jogos
         const data = await response.json();
+        const results: SearchGameResult[] = data.results;
+        const totalCount: number = data.count;
 
         // Retorne os dados dos jogos
-        return data.results;
+        return { results, totalCount };
     } catch (error) {
         console.error('Error fetching games:', error);
-        return []; // Retorna uma matriz vazia em caso de erro
+        return { results: [], totalCount: 0 }; // Retorna uma matriz vazia em caso de erro
     }
 };
 
